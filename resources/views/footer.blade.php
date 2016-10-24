45e6042c2ad99ae6569d73e5a1319213da7537d5
Jahid Mahmud <influencecse78@gmail.com>
2016-10-24 18:21:23 +0600

    Perfectly Working All

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div style="background-color: #000000; padding: 10px;">
-        <p style="text-align: center; color: white;">Copyright &copy OHSolutions.com</p>
+        <p style="text-align: center; color: white;">Copyright &copy Bdbreaker</p>
     </div>
 </footer>
 
